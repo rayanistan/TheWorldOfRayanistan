b60c19cc3fc5d8bdadc16b90529559c511f6e1f4
rayanistan <rayansri@gmail.com>
2016-09-17 12:19:51 -0700

    Added Weapon constructor, did not implement in main

---
+++ RayanMadLibs/src/com/company/weapon.java
@@ -0,0 +1,25 @@
+package com.company;
+
+/**
+ * Created by Rayanistan on 9/17/16.
+ */
+public class weapon {
+    public String weapon;
+    public String basic;
+    public String special;
+    public int basicdmg;
+    public int specialdmg;
+    public int specialuses;
+
+    public weapon(String weapon1, String basic1, String special1, int basicdmg1, int specialdmg1, int specialuses1){
+
+        weapon = weapon1;
+        basic = basic1;
+        special = special1;
+        basicdmg = basicdmg1;
+        specialdmg = specialdmg1;
+        specialuses = specialuses1;
+
+
+    }
+}
